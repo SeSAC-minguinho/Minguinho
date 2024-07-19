47f03da390b75fe43d8a2e812a69081766c28e1c
SabaPivot <58300587+SabaPivot@users.noreply.github.com>
2024-07-19 18:12:54 +0900

    Create extrract.py
    Initial Commit

---
+++ extrract.py
@@ -0,0 +1,27 @@
+# Import necessary libraries
+import os
+import glob
+import json
+
+# Read Files
+os.chdir(r'Path')
+directory = r'Directory'
+file_pattern = os.path.join(directory, '*.json')
+files = glob.glob(file_pattern)
+
+# Extract audio transcripts
+questions = []
+for file in files:
+    with open(file, mode="r", encoding="utf-8") as f:
+        data = json.load(f)
+        for i in range(len(data['list'])):
+            for j in range(len(data['list'][i]['list'])):
+                if 'audio' in data['list'][i]['list'][j].keys():
+                    for k in range(len(data['list'][i]['list'][j]['audio'])):
+                        if data['list'][i]['list'][j]['audio'][k]['type'] == 'Q':
+                            questions.append(data['list'][i]['list'][j]['audio'][k]['text'])
+
+# Save transcripts into the text file
+with open('questions.txt', 'w', encoding="utf-8") as f:
+    for q in questions:
+        f.write(f"{q}\n")
